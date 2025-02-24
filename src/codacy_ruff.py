a48287288d1b417d64ad299c0a466418d6fabc8f
david.marinho@codacy.com <david.marinho@codacy.com>
2025-02-24 18:11:34 +0000

    improved doc_generator and created tests

--- src/codacy_ruff.py
+++ src/codacy_ruff.py
@@ -3,11 +3,11 @@ import sys
 import json
 import jsonpickle
 import subprocess
-import ast
 import glob
 import signal
 from contextlib import contextmanager
 import traceback
+from pathlib import Path
 
 
 @contextmanager
@@ -56,9 +56,17 @@ def readJsonFile(path):
     return res
 
 
-def run_ruff(options, files, cwd=None):
-    cmd = ['ruff', 'check', '--output-format=json']
-    cmd = cmd + options + files
+def run_ruff(options, files, cwd=None, configFile=None):
+    print(configFile)
+    if configFile is not None:
+        cmd = ['ruff', 'check', '--output-format=json', '--config',configFile]
+        cmd = cmd + files
+    else:
+        cmd = ['ruff', 'check', '--output-format=json']
+        cmd = cmd + options + files
+    
+    print(cmd)
+    
     process = subprocess.Popen(
         cmd,
         stdout=subprocess.PIPE,
@@ -66,16 +74,16 @@ def run_ruff(options, files, cwd=None):
     )
     stdout = process.communicate()[0]
     result = stdout.decode('utf-8')
-    
+
     return result
 
 def chunks(lst, n):
     return [lst[i:i + n] for i in range(0, len(lst), n)]
 
 
-def run_ruff_parsed(options, files, cwd):
+def run_ruff_parsed(configFile, options, files, cwd):
     results = []
-    res = run_ruff(options, files, cwd)
+    res = run_ruff(options, files, cwd, configFile)
     ruff_dicts = json.loads(res)
     # a ruff_dict contains standardized keys:
     # {  'cell': None,
@@ -126,13 +134,24 @@ def readConfiguration(configFile, srcDir):
         options = []
     return (options, [f for f in files])
 
+def find_ruff_configFile(srcDir):
+    config_files = {"pyproject.toml", "ruff.toml", ".ruff.toml"}
+    root_files = list(Path(srcDir).iterdir())  # List only root directory files
+
+    for file in root_files:
+        if file.name in config_files and file.is_file():
+            return str(file) 
+    
+    return None  # Return None if no config file is found
+
 
 def run_tool(configFile, srcDir):
     (options, files) = readConfiguration(configFile, srcDir)
     res = []
     filesWithPath = [os.path.join(f) for f in files]
+    toolConfigFile = find_ruff_configFile(srcDir)
     for chunk in chunks(filesWithPath, 10):
-        res.extend(run_ruff_parsed(options, chunk, srcDir))
+        res.extend(run_ruff_parsed(toolConfigFile, options, chunk, srcDir))
 
     for result in res:
         if result.filename.startswith(srcDir):
@@ -148,7 +167,7 @@ def results_to_json(results):
 if __name__ == '__main__':
     with timeout(getTimeout(os.environ.get('TIMEOUT_SECONDS') or '')):
         try:
-            results = run_tool('/.codacyrc', 'src')
+            results = run_tool('/.codacyrc', '/src')
             results = results_to_json(results)
             print(results)
         except Exception:
